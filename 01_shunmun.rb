9a3358cc949570f7138967670d7a4e620f850c15
root <root@localhost.localdomain>
2018-04-29 00:51:01 +0900

    complete

--- 01_shunmun.rb
+++ 01_shunmun.rb
@@ -11,7 +11,7 @@ days_of_year = 366
 today.leap? if false
 days_of_year = 365
 
-p base
+p days_of_year
 
 # calcurate the Spring Equinox Day(shunbun_no_hi_in_Japanese)
 this_year = today.year
@@ -19,5 +19,14 @@ shunbun_day = 20.8431+0.242194*(this_year - 1980)-((this_year-1980)/4)
 shunbun_day=shunbun_day.floor
 
 p shunbun_day
+p today.year
 
+spring_equinox_day = Date.new(today.year,3,shunbun_day)
 
+p spring_equinox_day
+p today.to_date
+
+# calcurate how many days from Spring Equinox day...
+
+days_from_sp_eq_day = (today.to_date - spring_equinox_day).to_i
+p days_from_sp_eq_day
