3dcf868283d8faadc6bad5de6d863fda75a407aa
root <root@localhost.localdomain>
2018-04-29 00:22:34 +0900

    changed

--- 01_shunmun.rb
+++ 01_shunmun.rb
@@ -6,12 +6,18 @@ p today
 
 # leap or not?
 today.next_year.leap? if true
-base = 366
+days_of_year = 366
 
 today.leap? if false
-base = 365
+days_of_year = 365
 
 p base
 
+# calcurate the Spring Equinox Day(shunbun_no_hi_in_Japanese)
 this_year = today.year
+shunbun_day = 20.8431+0.242194*(this_year - 1980)-((this_year-1980)/4)
+shunbun_day=shunbun_day.floor
+
+p shunbun_day
+
 
